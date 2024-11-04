bf9baca3afbab66a961c760fcec15e2b7035d153
Mipmipp <sebastianhernandoh@gmail.com>
2024-11-04 12:52:24 -0300

    Fix CI/CD

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,7 +38,7 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: "npm run start",
+    command: "npm run dev",
     url: "http://127.0.0.1:5173",
     reuseExistingServer: !process.env.CI,
   },
