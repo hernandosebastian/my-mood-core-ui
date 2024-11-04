9a8712d3ee0850b3f221100a2f686fc530e41072
Mipmipp <sebastianhernandoh@gmail.com>
2024-11-04 13:22:04 -0300

    Fix webserver url

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,7 +39,7 @@ export default defineConfig({
   ],
   webServer: {
     command: "npm run dev",
-    url: "http://localhost:5174/",
+    url: "http://localhost:5173/",
     reuseExistingServer: !process.env.CI,
   },
 });
