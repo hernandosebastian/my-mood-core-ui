bd08838c7ccc844442607b2f1973c0e883736c27
Mipmipp <sebastianhernandoh@gmail.com>
2024-11-05 18:14:12 -0300

    Update main

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { SidebarProvider, SidebarTrigger } from "./components/ui/sidebar.tsx";
-import { AppSidebar } from "./components/app-sidebar.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <SidebarProvider>
-      <AppSidebar />
-      <main>
-        <SidebarTrigger />
-        <App />
-      </main>
-    </SidebarProvider>
+    <App />
   </StrictMode>
 );
