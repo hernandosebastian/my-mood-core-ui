5c838a9df3a5f068930b53e8fb7e411564564614
Mipmipp <sebastianhernandoh@gmail.com>
2024-11-06 11:56:53 -0300

    Update useGetMe hook with disabled param

--- src/features/authentication/hooks/useGetMe.tsx
+++ src/features/authentication/hooks/useGetMe.tsx
@@ -3,13 +3,16 @@ import { getMe } from "../services";
 import { IGetMeResponse } from "../dto";
 import { mapToUser } from "../mapper/mapToUser";
 import { User } from "../entity/User";
+import { getItem, StorageKeys } from "@/services/local-storage";
 
 const getMeKeys = (): string[] => ["getMe"];
 
 export const useGetMe = (): UseQueryResult<{ user: User }, unknown> => {
+  const accessToken = getItem(StorageKeys.COGNITO_ACCESS_TOKEN);
   return useQuery<IGetMeResponse, unknown, { user: User }>({
     queryKey: getMeKeys(),
     queryFn: getMe,
+    enabled: !!accessToken,
     select: ({
       id,
       username,
