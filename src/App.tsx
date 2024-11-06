bf358e5f566755067c5eb81b48794b6984d69a5b
Mipmipp <sebastianhernandoh@gmail.com>
2024-11-06 02:25:22 -0300

    Configurate react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { Routes } from "react-router-dom";
 import { RoutesList } from "./config/routes";
 import Layout from "./app/Layout";
+import { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-export default function App(): JSX.Element {
+const queryClient = new QueryClient();
+
+export default function App(): ReactNode {
   return (
     <Layout>
-      <Routes>{RoutesList}</Routes>
+      <QueryClientProvider client={queryClient}>
+        <Routes>{RoutesList}</Routes>
+      </QueryClientProvider>
     </Layout>
   );
 }
