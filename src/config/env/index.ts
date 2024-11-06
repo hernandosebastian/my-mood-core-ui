635d4a54a6047387c43e15e52767d94ad810b3be
Mipmipp <sebastianhernandoh@gmail.com>
2024-11-06 02:25:05 -0300

    Update environment variables

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -1,9 +1,33 @@
-export const env = {
+interface CognitoConfig {
+  userPoolId: string;
+  clientId: string;
+  endpoint: string;
+}
+
+interface CoreApiConfig {
+  baseUrl: string;
+}
+
+interface AppConfig {
+  baseUrl: string;
+}
+
+interface EnvConfig {
+  app: AppConfig;
+  coreApi: CoreApiConfig;
+  cognito: CognitoConfig;
+}
+
+export const env: EnvConfig = {
   app: {
     baseUrl: import.meta.env.PUBLIC_APP_BASE_URL,
   },
   coreApi: {
     baseUrl: import.meta.env.PUBLIC_CORE_API_BASE_URL,
   },
+  cognito: {
+    userPoolId: import.meta.env.PUBLIC_AWS_COGNITO_USER_POOL_ID,
+    clientId: import.meta.env.PUBLIC_AWS_COGNITO_CLIENT_ID,
+    endpoint: import.meta.env.PUBLIC_AWS_COGNITO_ENDPOINT,
+  },
 };
-
