25b70d36a8e0ab67ead4a394332d31890e623a36
Mipmipp <sebastianhernandoh@gmail.com>
2024-11-05 23:14:21 -0300

    Update routes

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,4 +1,30 @@
+import {
+  ConfirmPasswordPage,
+  ConfirmUserPage,
+  ForgotPasswordPage,
+  LogInPage,
+  ResendConfirmationCodePage,
+  SignUpPage,
+} from "@/features/authentication/pages";
 import { Route } from "react-router-dom";
 
-export const RoutesList = <Route path="*" element={<></>} />;
+const authRoutes = (
+  <>
+    <Route path="/log-in" element={<LogInPage />} />
+    <Route path="/sign-up" element={<SignUpPage />} />
+    <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+    <Route
+      path="/resend-confirmation-code"
+      element={<ResendConfirmationCodePage />}
+    />
+    <Route path="/confirm-password" element={<ConfirmPasswordPage />} />
+    <Route path="/confirm-user" element={<ConfirmUserPage />} />
+  </>
+);
 
+export const RoutesList = (
+  <>
+    {authRoutes}
+    <Route path="*" element={<></>} />
+  </>
+);
