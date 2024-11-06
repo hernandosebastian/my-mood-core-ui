995ba5425c2322196fa0eff28845f2486165cdf8
Mipmipp <sebastianhernandoh@gmail.com>
2024-11-06 12:52:57 -0300

    Reorder auth config

--- src/seo/config/authentication-config.ts
+++ src/seo/config/authentication-config.ts
@@ -1,22 +1,27 @@
 export const authenticationSeoConfig = {
-  forgotPassword: {
-    title: "My Mood - Forgot Password",
-    description:
-      "Reset your password and regain access to your My Mood account.",
+  confirmPassword: {
+    title: "My Mood - Confirm Password",
+    description: "Set your new password for My Mood and regain full access.",
   },
   confirmUser: {
     title: "My Mood - Confirm Your Account",
     description:
       "Confirm your account to complete registration and start using My Mood.",
   },
-  confirmPassword: {
-    title: "My Mood - Confirm Password",
-    description: "Set your new password for My Mood and regain full access.",
+  forgotPassword: {
+    title: "My Mood - Forgot Password",
+    description:
+      "Reset your password and regain access to your My Mood account.",
   },
   logIn: {
     title: "My Mood - Log In",
     description:
       "Log in to your My Mood account to track your mood and well-being.",
+  },
+  resendConfirmationCode: {
+    title: "My Mood - Resend Confirmation Code",
+    description:
+      "Resend the confirmation code to your My Mood account to complete the registration process.",
   },
   signUp: {
     title: "My Mood - Sign Up",
