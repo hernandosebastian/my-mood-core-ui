73713e651d8f37021044bdebbd576df426f9e1be
Mipmipp <sebastianhernandoh@gmail.com>
2024-11-10 17:26:06 -0300

    Add app header

--- src/app/Layout.tsx
+++ src/app/Layout.tsx
@@ -1,5 +1,7 @@
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/app-sidebar";
+import { AppSidebar } from "@/components/navbar/app-sidebar";
+
+import { AppHeader } from "@/components/navbar/app-header";
 
 export default function Layout({
   children,
@@ -9,9 +11,12 @@ export default function Layout({
   return (
     <SidebarProvider>
       <AppSidebar />
-      <SidebarInset className="px-4 py-6 sm:px-6 sm:py-8 lg:px-8 lg:py-10 justify-center">
-        {children}
-      </SidebarInset>
+      <div className="flex flex-1 flex-col">
+        <AppHeader />
+        <SidebarInset className="px-4 py-6 sm:px-6 sm:py-8 lg:px-8 lg:py-10 justify-center">
+          {children}
+        </SidebarInset>
+      </div>
     </SidebarProvider>
   );
 }
