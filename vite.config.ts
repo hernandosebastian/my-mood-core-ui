46dd5a20dbf82cba2f65063055e2749f9fb3668f
Mipmipp <sebastianhernandoh@gmail.com>
2024-11-09 21:30:26 -0300

    Add config to map relative paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,27 @@
-import path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+import tsconfig from './tsconfig.json';
 
 dotenv.config();
 
-// https://vite.dev/config/
+const rawAlias: { [key: string]: string[] } =
+  tsconfig.compilerOptions.paths || [];
+const alias = [] as { find: string; replacement: string }[];
+
+for (const key in rawAlias) {
+  alias.push({
+    find: key.replace('/*', ''),
+    replacement: path.resolve(__dirname, rawAlias[key][0].replace('/*', '')),
+  });
+}
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+    alias,
   },
   server: {
     host: true,
