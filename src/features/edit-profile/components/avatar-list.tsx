b5bbdf46570b4e1d59fd0a7c296ec3e0e8f336ac
Mipmipp <sebastianhernandoh@gmail.com>
2024-11-13 02:03:46 -0300

    Update avatar list to handle onLoading and onError

--- src/features/edit-profile/components/avatar-list.tsx
+++ src/features/edit-profile/components/avatar-list.tsx
@@ -1,18 +1,28 @@
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { IAvatar } from "../interfaces";
-import { ScrollArea } from "@radix-ui/react-scroll-area";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 interface IAvatarListProps {
   avatarList: IAvatar[];
   selectedAvatar: string | undefined;
   setSelectedAvatar: (src: string) => void;
+  setIsImageLoaded: (value: boolean) => void;
 }
 
 export function AvatarList({
   avatarList,
   selectedAvatar,
   setSelectedAvatar,
+  setIsImageLoaded,
 }: Readonly<IAvatarListProps>): JSX.Element {
+  const handleImageLoad = (): void => {
+    setIsImageLoaded(true);
+  };
+
+  const handleImageError = (): void => {
+    setIsImageLoaded(false);
+  };
+
   return (
     <ScrollArea className="h-[300px] border rounded-lg p-4">
       <div className="grid grid-cols-4 gap-4 sm:grid-cols-5 md:grid-cols-6 lg:grid-cols-8">
@@ -30,6 +40,8 @@ export function AvatarList({
               <AvatarImage
                 src={avatar.src}
                 alt={`Avatar option ${avatar.name}`}
+                onLoad={handleImageLoad}
+                onError={handleImageError}
               />
               <AvatarFallback className="text-gray-500">
                 {avatar.name}
