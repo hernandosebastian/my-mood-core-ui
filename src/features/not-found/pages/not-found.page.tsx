ff2ef904f9b0c5f2d842eda2a9c0d857452f7f04
Mipmipp <sebastianhernandoh@gmail.com>
2024-11-12 23:41:05 -0300

    Update not found page with SEO hook

--- src/features/not-found/pages/not-found.page.tsx
+++ src/features/not-found/pages/not-found.page.tsx
@@ -1,6 +1,13 @@
+import { useSEO } from "@/seo/hooks";
 import { NotFound } from "../components";
+import { notFoundSeoConfig } from "@/seo/config";
 
 export function NotFoundPage(): JSX.Element {
+  useSEO({
+    title: notFoundSeoConfig.title,
+    description: notFoundSeoConfig.description,
+  });
+
   return <NotFound />;
 }
 
