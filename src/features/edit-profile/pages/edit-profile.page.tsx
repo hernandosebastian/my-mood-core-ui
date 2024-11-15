388d5c2890b83de5c0abdad2a76aa31f0ee19af7
Mipmipp <sebastianhernandoh@gmail.com>
2024-11-14 22:24:04 -0300

    Add edit profile logic to handle form

--- src/features/edit-profile/pages/edit-profile.page.tsx
+++ src/features/edit-profile/pages/edit-profile.page.tsx
@@ -1,10 +1,16 @@
-import { Button } from "@/components/ui/button";
-import { CurrentAvatar, AvatarList, AttributionLink } from "../components";
-import { Loading } from "@/components/common/Loading";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Loading } from "@/components/common/Loading";
+import { EditProfileForm } from "../components";
 import { useAvatars, useProfile } from "../hooks";
+import { useToast } from "@/hooks";
+import { useUpdateProfile } from "../hooks/use-update-profile";
 import { useSEO } from "@/seo/hooks";
 import { EditProfileSeoConfig } from "@/seo/config/edit-profile.config";
+import { z } from "zod";
+import { editProfileSchema } from "../schemas/";
+import { EditProfileToastMessages } from "../messages/edit-profile.messages";
+import { AxiosError } from "axios";
 
 export function EditProfilePage(): JSX.Element {
   useSEO({
@@ -12,19 +18,50 @@ export function EditProfilePage(): JSX.Element {
     description: EditProfileSeoConfig.description,
   });
 
-  const { getMeQuery, currentAvatar, setCurrentAvatar } = useProfile();
-  const { avatarList, isImageLoaded, setIsImageLoaded } = useAvatars();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [selectedAvatar, setSelectedAvatar] = useState<string | undefined>(
     undefined
   );
 
-  const handleSave = (): void => {
-    setCurrentAvatar(selectedAvatar);
-    console.log("Profile updated with new avatar:", selectedAvatar);
-  };
+  const { getMeQuery, currentAvatar } = useProfile();
+  const { avatarList, isImageLoaded } = useAvatars();
+  const { showSuccessToast, showErrorToast } = useToast();
+  const updateProfileMutation = useUpdateProfile();
+  const navigate = useNavigate();
 
-  if (getMeQuery.isLoading) {
-    return <Loading />;
+  if (getMeQuery.isLoading) return <Loading />;
+
+  async function onSubmit(
+    values: z.infer<typeof editProfileSchema>
+  ): Promise<void> {
+    setIsLoading(true);
+
+    if (values.nickname === getMeQuery.data?.user.nickname) {
+      const { nickname, ...valuesWithoutNickname } = values;
+      values = valuesWithoutNickname;
+    }
+
+    try {
+      await updateProfileMutation.mutateAsync(values, {
+        onSuccess: () => {
+          showSuccessToast(
+            EditProfileToastMessages.success.title,
+            EditProfileToastMessages.success.description
+          );
+          navigate("/");
+        },
+        onError: (error: AxiosError) => {
+          const errorMessage = (error.response?.data as { message?: string })
+            ?.message;
+          showErrorToast(
+            EditProfileToastMessages.error.title,
+            errorMessage ?? EditProfileToastMessages.error.description
+          );
+        },
+      });
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   return (
@@ -36,29 +73,17 @@ export function EditProfilePage(): JSX.Element {
       </header>
       <main className="flex-grow container mx-auto px-4 py-8">
         <div className="max-w-3xl mx-auto space-y-8">
-          <CurrentAvatar
+          <EditProfileForm
+            isLoading={isLoading}
             currentAvatar={currentAvatar}
             selectedAvatar={selectedAvatar}
+            avatarList={avatarList}
             isImageLoaded={isImageLoaded}
-            setIsImageLoaded={setIsImageLoaded}
+            initialNickname={getMeQuery.data?.user.nickname ?? ""}
+            initialAvatarSrc={getMeQuery.data?.user.avatarSrc ?? ""}
+            onSubmit={onSubmit}
+            setSelectedAvatar={setSelectedAvatar}
           />
-
-          <div>
-            <h2 className="text-xl font-semibold text-gray-800 mb-4">
-              Choose a new avatar:
-            </h2>
-            <AvatarList
-              avatarList={avatarList}
-              selectedAvatar={selectedAvatar}
-              setSelectedAvatar={setSelectedAvatar}
-              setIsImageLoaded={setIsImageLoaded}
-            />
-          </div>
-          <Button onClick={handleSave} className="w-full">
-            Save Changes
-          </Button>
-
-          <AttributionLink />
         </div>
       </main>
     </div>
