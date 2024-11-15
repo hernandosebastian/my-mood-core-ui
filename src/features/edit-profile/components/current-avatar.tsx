08b7c71bfd3d37eefd7dce481ffe14305320f224
Mipmipp <sebastianhernandoh@gmail.com>
2024-11-14 22:21:27 -0300

    Update current avatar to don't get unnecessary props

--- src/features/edit-profile/components/current-avatar.tsx
+++ src/features/edit-profile/components/current-avatar.tsx
@@ -1,40 +1,23 @@
-import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 
 interface ICurrentAvatarProps {
   currentAvatar: string | undefined;
   selectedAvatar: string | undefined;
   isImageLoaded: boolean;
-  setIsImageLoaded: (value: boolean) => void;
 }
 
 export function CurrentAvatar({
   currentAvatar,
   selectedAvatar,
-  isImageLoaded,
-  setIsImageLoaded,
 }: Readonly<ICurrentAvatarProps>): JSX.Element {
-  const handleImageLoad = (): void => {
-    setIsImageLoaded(true);
-  };
-
-  const handleImageError = (): void => {
-    setIsImageLoaded(false);
-  };
-
   return (
     <div className="flex flex-col items-center space-y-4">
       <h2 className="text-lg text-gray-800">Current Avatar</h2>
       <Avatar className="w-32 h-32 bg-gray-300">
-        {isImageLoaded ? (
-          <AvatarImage
-            src={selectedAvatar ?? currentAvatar}
-            alt="Current profile picture"
-            onLoad={handleImageLoad}
-            onError={handleImageError}
-          />
-        ) : (
-          <AvatarFallback className="text-gray-500">User</AvatarFallback>
-        )}
+        <AvatarImage
+          src={selectedAvatar ?? currentAvatar}
+          alt="Current profile picture"
+        />
       </Avatar>
     </div>
   );
