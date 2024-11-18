945e308f77663cac2b456fd0a0269cf907b08f64
Mipmipp <sebastianhernandoh@gmail.com>
2024-11-18 19:06:58 -0300

    Add edit profile tests

---
+++ playwright/tests/features/edit-profile/edit-profile.spec.ts
@@ -0,0 +1,267 @@
+import { test, expect } from "@playwright/test";
+import dotenv from "dotenv";
+import { editProfileToastMessages } from "@/features/edit-profile/messages/edit-profile.messages";
+import { successGetMeFixture } from "../../../fixtures/features/authentication/get-me.fixture";
+import {
+  errorEditProfileFixtureWithoutMessage,
+  errorEditProfileNicknameTakenFixture,
+  successEditProfileFixture,
+  successEditProfileOnlyAvatarFixture,
+  successEditProfileOnlyNicknameFixture,
+} from "../../../fixtures/features/edit-profile/edit-profile.fixture";
+
+import { logIn } from "utils/log-in";
+
+dotenv.config();
+
+const BASE_URL = process.env.VITE_APP_BASE_URL || "http://localhost:5173/";
+
+test.beforeEach(async ({ page, isMobile }) => {
+  await page.goto(`${BASE_URL}`);
+  await logIn({ page, isMobile });
+});
+
+test.describe("feature/edit-profile", () => {
+  test("should successfully submit the form with new avatar and nickname", async ({
+    page,
+    isMobile,
+  }) => {
+    await page.route("**/api/v1/user/me", (route) => {
+      if (route.request().method() === "GET") {
+        route.fulfill(successGetMeFixture);
+      } else if (route.request().method() === "PATCH") {
+        route.fulfill(successEditProfileFixture);
+      } else {
+        route.continue();
+      }
+    });
+
+    if (isMobile) {
+      await page.getByTestId("toggle-sidebar-trigger").click();
+    }
+
+    await page.getByTestId("sidebar-open-menu-button").click();
+    await page.getByTestId("sidebar-edit-profile-menu-item").click();
+
+    if (isMobile) {
+      await page.getByTestId("toggle-sidebar-trigger-close-responsive").click();
+    }
+
+    const currentAvatar = page.getByTestId("current-avatar");
+    const nicknameInput = page.getByTestId("edit-profile-nickname");
+
+    const getMeBody = JSON.parse(successGetMeFixture.body);
+    await expect(currentAvatar).toHaveAttribute("src", getMeBody.avatarSrc);
+    await expect(nicknameInput).toHaveValue(getMeBody.nickname);
+
+    await page.getByTestId("avatar-Multiavatar-0af9e888d36c86d96f").click();
+    await nicknameInput.click();
+    await nicknameInput.fill("TestNickname");
+
+    const responseEditProfileBody = JSON.parse(successEditProfileFixture.body);
+    await expect(currentAvatar).toHaveAttribute(
+      "src",
+      responseEditProfileBody.avatarSrc
+    );
+    await expect(nicknameInput).toHaveValue(responseEditProfileBody.nickname);
+
+    await page.getByTestId("edit-profile-submit-button").click();
+
+    await expect(
+      page.getByText(editProfileToastMessages.success.title)
+    ).toBeVisible();
+    await expect(
+      page.getByText(editProfileToastMessages.success.description)
+    ).toBeVisible();
+
+    await expect(page).toHaveURL(`${BASE_URL}`);
+  });
+
+  test("should successfully submit the form with new avatar", async ({
+    page,
+    isMobile,
+  }) => {
+    await page.route("**/api/v1/user/me", (route) => {
+      if (route.request().method() === "GET") {
+        route.fulfill(successGetMeFixture);
+      } else if (route.request().method() === "PATCH") {
+        route.fulfill(successEditProfileOnlyAvatarFixture);
+      } else {
+        route.continue();
+      }
+    });
+
+    if (isMobile) {
+      await page.getByTestId("toggle-sidebar-trigger").click();
+    }
+
+    await page.getByTestId("sidebar-open-menu-button").click();
+    await page.getByTestId("sidebar-edit-profile-menu-item").click();
+
+    if (isMobile) {
+      await page.getByTestId("toggle-sidebar-trigger-close-responsive").click();
+    }
+
+    const currentAvatar = page.getByTestId("current-avatar");
+    const nicknameInput = page.getByTestId("edit-profile-nickname");
+
+    const getMeBody = JSON.parse(successGetMeFixture.body);
+    await expect(currentAvatar).toHaveAttribute("src", getMeBody.avatarSrc);
+    await expect(nicknameInput).toHaveValue(getMeBody.nickname);
+
+    await page.getByTestId("avatar-Multiavatar-0af9e888d36c86d96f").click();
+
+    const responseEditProfileBody = JSON.parse(
+      successEditProfileOnlyAvatarFixture.body
+    );
+    await expect(currentAvatar).toHaveAttribute(
+      "src",
+      responseEditProfileBody.avatarSrc
+    );
+    await expect(nicknameInput).toHaveValue(getMeBody.nickname);
+
+    await page.getByTestId("edit-profile-submit-button").click();
+
+    await expect(
+      page.getByText(editProfileToastMessages.success.title)
+    ).toBeVisible();
+    await expect(
+      page.getByText(editProfileToastMessages.success.description)
+    ).toBeVisible();
+
+    await expect(page).toHaveURL(`${BASE_URL}`);
+  });
+
+  test("should successfully submit the form with nickname", async ({
+    page,
+    isMobile,
+  }) => {
+    await page.route("**/api/v1/user/me", (route) => {
+      if (route.request().method() === "GET") {
+        route.fulfill(successGetMeFixture);
+      } else if (route.request().method() === "PATCH") {
+        route.fulfill(successEditProfileOnlyNicknameFixture);
+      } else {
+        route.continue();
+      }
+    });
+
+    if (isMobile) {
+      await page.getByTestId("toggle-sidebar-trigger").click();
+    }
+
+    await page.getByTestId("sidebar-open-menu-button").click();
+    await page.getByTestId("sidebar-edit-profile-menu-item").click();
+
+    if (isMobile) {
+      await page.getByTestId("toggle-sidebar-trigger-close-responsive").click();
+    }
+
+    const currentAvatar = page.getByTestId("current-avatar");
+    const nicknameInput = page.getByTestId("edit-profile-nickname");
+
+    const getMeBody = JSON.parse(successGetMeFixture.body);
+    await expect(currentAvatar).toHaveAttribute("src", getMeBody.avatarSrc);
+    await expect(nicknameInput).toHaveValue(getMeBody.nickname);
+
+    await nicknameInput.click();
+    await nicknameInput.fill("TestNickname");
+
+    const responseEditProfileBody = JSON.parse(
+      successEditProfileOnlyNicknameFixture.body
+    );
+    await expect(currentAvatar).toHaveAttribute("src", getMeBody.avatarSrc);
+    await expect(nicknameInput).toHaveValue(responseEditProfileBody.nickname);
+
+    await page.getByTestId("edit-profile-submit-button").click();
+
+    await expect(
+      page.getByText(editProfileToastMessages.success.title)
+    ).toBeVisible();
+    await expect(
+      page.getByText(editProfileToastMessages.success.description)
+    ).toBeVisible();
+
+    await expect(page).toHaveURL(`${BASE_URL}`);
+  });
+
+  test("should get error if nickname is taken", async ({ page, isMobile }) => {
+    await page.route("**/api/v1/user/me", (route) => {
+      if (route.request().method() === "GET") {
+        route.fulfill(successGetMeFixture);
+      } else if (route.request().method() === "PATCH") {
+        route.fulfill(errorEditProfileNicknameTakenFixture);
+      } else {
+        route.continue();
+      }
+    });
+
+    if (isMobile) {
+      await page.getByTestId("toggle-sidebar-trigger").click();
+    }
+
+    await page.getByTestId("sidebar-open-menu-button").click();
+    await page.getByTestId("sidebar-edit-profile-menu-item").click();
+
+    if (isMobile) {
+      await page.getByTestId("toggle-sidebar-trigger-close-responsive").click();
+    }
+
+    const nicknameInput = page.getByTestId("edit-profile-nickname");
+
+    await nicknameInput.click();
+    await nicknameInput.fill("NicknameTaken");
+
+    await page.getByTestId("edit-profile-submit-button").click();
+
+    const errorResponseBody = JSON.parse(
+      errorEditProfileNicknameTakenFixture.body
+    );
+
+    await expect(
+      page.getByText(editProfileToastMessages.error.title)
+    ).toBeVisible();
+    await expect(page.getByText(errorResponseBody.message)).toBeVisible();
+  });
+
+  test("should display default error message if there is no error message in the body", async ({
+    page,
+    isMobile,
+  }) => {
+    await page.route("**/api/v1/user/me", (route) => {
+      if (route.request().method() === "GET") {
+        route.fulfill(successGetMeFixture);
+      } else if (route.request().method() === "PATCH") {
+        route.fulfill(errorEditProfileFixtureWithoutMessage);
+      } else {
+        route.continue();
+      }
+    });
+
+    if (isMobile) {
+      await page.getByTestId("toggle-sidebar-trigger").click();
+    }
+
+    await page.getByTestId("sidebar-open-menu-button").click();
+    await page.getByTestId("sidebar-edit-profile-menu-item").click();
+
+    if (isMobile) {
+      await page.getByTestId("toggle-sidebar-trigger-close-responsive").click();
+    }
+
+    const nicknameInput = page.getByTestId("edit-profile-nickname");
+
+    await nicknameInput.click();
+    await nicknameInput.fill("NicknameError");
+
+    await page.getByTestId("edit-profile-submit-button").click();
+
+    await expect(
+      page.getByText(editProfileToastMessages.error.title)
+    ).toBeVisible();
+    await expect(
+      page.getByText(editProfileToastMessages.error.description)
+    ).toBeVisible();
+  });
+});
+
