b2be756453d24ddec2d7485ed7212f2b34f95a22
Mipmipp <sebastianhernandoh@gmail.com>
2024-11-21 22:20:50 -0300

    Refactor calendar to single calendar

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,5 +1,5 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons";
-import { DayPicker, DayPickerProps } from "react-day-picker";
+import { DayPicker, DayPickerSingleProps } from "react-day-picker";
 
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "./buttonVariants";
@@ -11,16 +11,18 @@ export const IconRight = (): JSX.Element => (
   <ChevronRightIcon className="h-4 w-4" />
 );
 
-export type CalendarProps = DayPickerProps & {
+export type CalendarProps = DayPickerSingleProps & {
   className?: string;
   classNames?: Partial<Record<string, string>>;
   showOutsideDays?: boolean;
 };
 
-function Calendar({
+function SingleCalendar({
   className,
   classNames,
+  selected,
   showOutsideDays = true,
+  onDayClick,
   ...props
 }: CalendarProps): JSX.Element {
   return (
@@ -46,9 +48,7 @@ function Calendar({
         row: "flex w-full mt-2",
         cell: cn(
           "relative p-0 text-center text-sm focus-within:relative focus-within:z-20 [&:has([aria-selected])]:bg-slate-100 [&:has([aria-selected].day-outside)]:bg-slate-100/50 [&:has([aria-selected].day-range-end)]:rounded-r-md dark:[&:has([aria-selected])]:bg-slate-800 dark:[&:has([aria-selected].day-outside)]:bg-slate-800/50",
-          props.mode === "range"
-            ? "[&:has(>.day-range-end)]:rounded-r-md [&:has(>.day-range-start)]:rounded-l-md first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md"
-            : "[&:has([aria-selected])]:rounded-md"
+          "[&:has([aria-selected])]:rounded-md"
         ),
         day: cn(
           buttonVariants({ variant: "ghost" }),
@@ -57,7 +57,7 @@ function Calendar({
         day_range_start: "day-range-start",
         day_range_end: "day-range-end",
         day_selected:
-          "bg-slate-900 text-slate-50 hover:bg-slate-900 hover:text-slate-50 focus:bg-slate-900 focus:text-slate-50 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50 dark:hover:text-slate-900 dark:focus:bg-slate-50 dark:focus:text-slate-900",
+          "bg-slate-900 text-slate-100 hover:bg-slate-900 hover:text-slate-100 focus:bg-slate-900 focus:text-slate-50 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50 dark:hover:text-slate-900 dark:focus:bg-slate-50 dark:focus:text-slate-900",
         day_today:
           "bg-slate-100 text-slate-900 dark:bg-slate-800 dark:text-slate-50",
         day_outside:
@@ -72,10 +72,12 @@ function Calendar({
         IconLeft,
         IconRight,
       }}
+      selected={selected}
+      onDayClick={onDayClick}
       {...props}
     />
   );
 }
-Calendar.displayName = "Calendar";
+SingleCalendar.displayName = "SingleCalendar";
 
-export { Calendar };
+export { SingleCalendar };
