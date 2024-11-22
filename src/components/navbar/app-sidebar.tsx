099633dd97f956bc1c1f32fbcc007df3f40163f1
Mipmipp <sebastianhernandoh@gmail.com>
2024-11-21 22:49:33 -0300

    Improve sidebar when data is loading

--- src/components/navbar/app-sidebar.tsx
+++ src/components/navbar/app-sidebar.tsx
@@ -15,6 +15,7 @@ import { NavUserLogged } from "./nav-user-logged";
 import { SidebarContentUserAnonymous } from "./sidebar-content-user-anonymous";
 import { SidebarContentUserLogged } from "./sidebar-content-user-logged";
 import { useSidebar } from "@/hooks";
+import { Loader2 } from "lucide-react";
 
 export function AppSidebar({
   ...props
@@ -22,6 +23,32 @@ export function AppSidebar({
   const { isMobile } = useSidebar();
   const getMeQuery = useGetMe();
   const isLoggedIn = getMeQuery.data?.user !== undefined;
+
+  if (getMeQuery.isLoading) {
+    return (
+      <Sidebar {...props} id="sidebar">
+        {isMobile && (
+          <div className="p-2 flex items-center border-b border-sidebar-border">
+            <SidebarTrigger
+              className="p-2 rounded-full hover:bg-sidebar-accent transition-all"
+              closeIcon={true}
+              data-testid="toggle-sidebar-trigger-close-responsive"
+            />
+          </div>
+        )}
+        <SidebarHeader className="h-16 border-b border-sidebar-border justify-center items-center">
+          <Loader2 className="h-6 w-6 animate-spin text-inherit" />
+        </SidebarHeader>
+        <SidebarContent className="justify-center items-center">
+          <Loader2 className="h-6 w-6 animate-spin text-inherit" />
+        </SidebarContent>
+        <SidebarContent>
+          <NavSecondary />
+        </SidebarContent>
+        <NavBarFooter />
+      </Sidebar>
+    );
+  }
 
   return (
     <Sidebar {...props} id="sidebar">
