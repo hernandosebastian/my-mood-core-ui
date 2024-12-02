8a2c1fcb701191fa0a5a8ab9b78a85f5ef3035e2
Mipmipp <sebastianhernandoh@gmail.com>
2024-12-01 23:43:40 -0300

    Add interfaces and dtos

---
+++ src/features/track/interfaces/index.ts
@@ -0,0 +1,4 @@
+export type { ICreateTrackDto } from "./create-track.dto.interface";
+export type { IGetTrackByDateRangeDto } from "./get-track-by-date-range.dto.interface";
+export type { ITrack } from "./track.interface";
+
