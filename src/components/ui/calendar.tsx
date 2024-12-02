03d919212e3d70e37522cbebecf853e1144ec90b
Mipmipp <sebastianhernandoh@gmail.com>
2024-12-01 23:36:30 -0300

    Update single calendar to handle disabled prop

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -23,12 +23,15 @@ function SingleCalendar({
   selected,
   showOutsideDays = true,
   onDayClick,
+  disabled = false,
   ...props
 }: CalendarProps): JSX.Element {
   return (
     <DayPicker
       showOutsideDays={showOutsideDays}
-      className={cn("p-3", className)}
+      className={cn("p-3", className, {
+        "pointer-events-none opacity-50": disabled,
+      })}
       classNames={{
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
