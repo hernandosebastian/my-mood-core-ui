3a0c7c9e3f22568449dbf25d440d53b8e6055b46
Mipmipp <sebastianhernandoh@gmail.com>
2024-12-01 23:37:40 -0300

    Update date picker to update te date states on click

--- src/features/calendar/components/date-picker.tsx
+++ src/features/calendar/components/date-picker.tsx
@@ -1,10 +1,44 @@
 import { SingleCalendar } from "@/components/ui/calendar";
 import { SidebarGroup, SidebarGroupContent } from "@/components/ui/sidebar";
-import { useSelectedDate, useSetSelectedDate } from "../hooks";
+import {
+  useMonthDateRange,
+  useSelectedDate,
+  useSetSelectedDate,
+} from "../hooks";
+import { useNavigate } from "react-router-dom";
+import { calendarToastMessages } from "../messages/calendar.messages";
+import { useToast } from "@/hooks";
+import { useGetTrackByDateRange } from "@/features/track/hooks";
 
 export function DatePicker(): JSX.Element {
+  const navigate = useNavigate();
+  const { showErrorToast } = useToast();
+
+  const { month, year, startDate, endDate } = useMonthDateRange();
   const selectedDate = useSelectedDate();
   const setSelectedDate = useSetSelectedDate();
+
+  const { isLoading, error } = useGetTrackByDateRange({
+    month,
+    year,
+    startDate,
+    endDate,
+  });
+
+  if (error) {
+    const errorMessage = (error.response?.data as { message?: string })
+      ?.message;
+
+    showErrorToast(
+      calendarToastMessages.error.title,
+      errorMessage ?? calendarToastMessages.error.description
+    );
+  }
+
+  const handleOnDayClick = (date: Date): void => {
+    setSelectedDate(date);
+    navigate("/track");
+  };
 
   return (
     <SidebarGroup className="px-0" data-testid="sidebar-date-picker">
@@ -13,7 +47,8 @@ export function DatePicker(): JSX.Element {
           className="[&_[role=gridcell].bg-accent]:bg-sidebar-primary [&_[role=gridcell].bg-accent]:text-sidebar-primary-foreground [&_[role=gridcell]]:w-[33px]"
           selected={selectedDate}
           mode="single"
-          onDayClick={setSelectedDate}
+          onDayClick={handleOnDayClick}
+          disabled={isLoading}
         />
       </SidebarGroupContent>
     </SidebarGroup>
