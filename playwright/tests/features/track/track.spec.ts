c0efe633dd8f7ceaa4995c51d740e1a19484ea6e
Mipmipp <sebastianhernandoh@gmail.com>
2024-12-09 23:54:45 -0300

    tests: cover cases where get day's track fails

--- playwright/tests/features/track/track.spec.ts
+++ playwright/tests/features/track/track.spec.ts
@@ -1,5 +1,6 @@
 import { expect, test } from "@playwright/test";
 import dotenv from "dotenv";
+import { calendarToastMessages } from "@/features/calendar/messages/calendar.messages";
 import {
   createTrackToastMessages,
   createTrackErrorMessages,
@@ -47,6 +48,62 @@ test.beforeEach(async ({ page, isMobile }) => {
 
   await page.goto(`${BASE_URL}`);
   await logIn({ page, isMobile, isSidebarOpen: false });
+});
+
+test.describe("features/track - get (errors)", () => {
+  test("should display error message from body if there is one", async ({
+    page,
+    isMobile,
+  }) => {
+    await page.route(
+      "**/api/v1/track/by-date-range?startDate=2024-10-01T03:00:00.000Z&endDate=2024-11-01T02:59:59.999Z",
+      (route) => {
+        route.fulfill(errorGetTrackFixtureWithMessage);
+      }
+    );
+
+    await openSidebarIfMobile({ page, isMobile });
+    await selectDayFromCalendar({ page, dayNumber: 10 });
+    await closeSidebarIfMobile({ page, isMobile });
+
+    const errorResponseBody = JSON.parse(errorGetTrackFixtureWithMessage.body);
+
+    const errorToastMessageTitle = page.getByText(
+      calendarToastMessages.error.title
+    );
+    const errorToastMessageDescription = page.getByText(
+      errorResponseBody.message
+    );
+
+    await expect(errorToastMessageTitle).toBeVisible();
+    await expect(errorToastMessageDescription).toBeVisible();
+  });
+
+  test("should display default error message if there is no error message in the body", async ({
+    page,
+    isMobile,
+  }) => {
+    await page.route(
+      "**/api/v1/track/by-date-range?startDate=2024-10-01T03:00:00.000Z&endDate=2024-11-01T02:59:59.999Z",
+      (route) => {
+        route.fulfill(errorGetTrackFixtureWithoutMessage);
+      }
+    );
+
+    await openSidebarIfMobile({ page, isMobile });
+    await selectDayFromCalendar({ page, dayNumber: 10 });
+    await closeSidebarIfMobile({ page, isMobile });
+
+    const errorToastMessageTitle = page.getByText(
+      calendarToastMessages.error.title
+    );
+    const errorToastMessageDescription = page.getByText(
+      calendarToastMessages.error.description
+    );
+
+    await expect(errorToastMessageTitle).toBeVisible();
+    await expect(errorToastMessageDescription).toBeVisible();
+  });
 });
 
 test.describe("features/track - create", () => {
