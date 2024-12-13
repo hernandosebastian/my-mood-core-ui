bc55e9cca805c95e0c4b83cb729c30225b53cbd5
Mipmipp <sebastianhernandoh@gmail.com>
2024-12-12 23:20:15 -0300

    refactor: update previous and next carousel button styles

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -221,9 +221,9 @@ const CarouselPrevious = React.forwardRef<
       variant={variant}
       size={size}
       className={cn(
-        "absolute  h-8 w-8 rounded-full",
+        " h-8 w-8 rounded-full mt-auto mb-auto",
         orientation === "horizontal"
-          ? "-left-12 top-1/2 -translate-y-1/2"
+          ? "-left-12 top-1/2"
           : "-top-12 left-1/2 -translate-x-1/2 rotate-90",
         className
       )}
@@ -254,9 +254,9 @@ const CarouselNext = React.forwardRef<
       variant={variant}
       size={size}
       className={cn(
-        "absolute h-8 w-8 rounded-full",
+        "h-8 w-8 rounded-full mt-auto mb-auto",
         orientation === "horizontal"
-          ? "-right-12 top-1/2 -translate-y-1/2"
+          ? "-right-12 top-1/2"
           : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
         className
       )}
