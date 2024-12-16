07b94c24e0d88fa697f20b7c8ffc59f9eab456ff
Mipmipp <sebastianhernandoh@gmail.com>
2024-12-16 00:15:54 -0300

    refactor: improve paddings styles

--- src/features/homepage/components/homepage-hero.tsx
+++ src/features/homepage/components/homepage-hero.tsx
@@ -1,21 +1,40 @@
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
+import { HomepageOpenSourceButton } from "./homepage-open-source-button";
+import { buttonVariants } from "@/components/ui/buttonVariants";
+import { env } from "@/config/env";
 
 export function HomepageHero(): JSX.Element {
   return (
     <div className="space-y-6 text-center">
-      <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl text-gray-900">
+      <HomepageOpenSourceButton />
+
+      <h1 className="text-5xl font-bold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl text-gray-900">
         My Mood
       </h1>
-      <p className="text-xl text-gray-600 max-w-[600px] mx-auto">
+      <p className="text-2xl text-gray-600 max-w-[600px] mx-auto">
         Track your daily emotions, monitor patterns over time, and gain insights
         into your mood journey.
       </p>
-      <Button asChild size="lg" className="mt-4">
-        <Link to="/log-in" data-testid="homepage-get-started-button">
-          Get Started
-        </Link>
-      </Button>
+      <div className="flex gap-4 justify-center mt-2">
+        <Button asChild size="lg" className="w-32">
+          <Link to="/log-in" data-testid="homepage-get-started-button">
+            Get Started
+          </Link>
+        </Button>
+        <a
+          data-testid="homepage-github-button"
+          className={`w-32 ${buttonVariants({
+            variant: "secondary",
+            size: "lg",
+          })}`}
+          href={env.githubProfiles.projectRepositoryGithub}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>
+      </div>
     </div>
   );
 }
--- src/features/homepage/pages/homepage.page.tsx
+++ src/features/homepage/pages/homepage.page.tsx
@@ -1,21 +1,16 @@
-import { Separator } from "@/components/ui/separator";
 import {
   HomepageHero,
-  HomepageImage,
-  HomepageOpenSourceButton,
+  HomepageVideo,
   HomepageWhyTrackYourMood,
 } from "../components";
+
 export function Homepage(): JSX.Element {
   return (
     <div className="min-h-screen flex flex-col" data-testid="homepage-section">
-      <main className="flex-grow flex flex-col items-center justify-center space-y-12 px-4 py-16 bg-gradient-to-b from-background to-secondary/20">
-        <HomepageOpenSourceButton />
+      <main className="flex-grow flex flex-col items-center justify-center space-y-16 px-4 py-16 bg-gradient-to-b from-background to-secondary/20">
         <HomepageHero />
-        <HomepageImage />
+        <HomepageVideo />
       </main>
-
-      <Separator className="bg-gray-200" />
-
       <HomepageWhyTrackYourMood />
     </div>
   );
