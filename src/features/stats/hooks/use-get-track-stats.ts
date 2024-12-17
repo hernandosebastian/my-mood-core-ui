eff10c387e36c9530a8ecac7ad46a97966957310
Mipmipp <sebastianhernandoh@gmail.com>
2024-12-17 18:37:02 -0300

    feature: add get track stats hook

---
+++ src/features/stats/hooks/use-get-track-stats.ts
@@ -0,0 +1,40 @@
+import { useQuery, UseQueryResult } from "react-query";
+import {
+  IGetTrackStatsResponse,
+  IMapTrackStatsToMoodTrackingResponse,
+} from "../interfaces";
+import { AxiosError } from "axios";
+import { getItem, StorageKeys } from "@/services/local-storage";
+import { getTrackStats } from "../services/get-track-stats.service";
+import { mapTrackStatsToMoodTracking } from "../mapper";
+
+export const trackStatsKeys = {
+  stats: (): string => "stats",
+};
+
+export const useGetTrackByDateRange = (): UseQueryResult<
+  IMapTrackStatsToMoodTrackingResponse[] | undefined,
+  AxiosError
+> => {
+  const accessToken = getItem(StorageKeys.COGNITO_ACCESS_TOKEN);
+
+  return useQuery<
+    IGetTrackStatsResponse,
+    AxiosError,
+    IMapTrackStatsToMoodTrackingResponse[] | undefined
+  >({
+    queryKey: "stats",
+    queryFn: async () => getTrackStats(),
+    enabled: !!accessToken,
+    select: (response: IGetTrackStatsResponse) => {
+      if (
+        response.totalTrackStats.length > 0 ||
+        response.tracksLast3MonthsStats.length > 0
+      ) {
+        return [mapTrackStatsToMoodTracking(response)];
+      }
+      return undefined;
+    },
+  });
+};
+
