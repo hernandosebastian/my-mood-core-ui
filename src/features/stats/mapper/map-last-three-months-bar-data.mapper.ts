00851b55a2d716a68bf1a0564cddd7da8e3114b0
hernandosebastian <sebastianhernando.dev@gmail.com>
2024-12-19 17:39:54 -0300

    refactor: move monthsNames to a constants utils

--- src/features/stats/mapper/map-last-three-months-bar-data.mapper.ts
+++ src/features/stats/mapper/map-last-three-months-bar-data.mapper.ts
@@ -1,25 +1,11 @@
 import { IMonthData, ITrackMonthlyStats } from "../interfaces";
-
-const monthNames = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
+import { monthsNames } from "../utils";
 
 export const mapLastThreeMonthsBarData = (
   tracksLast3MonthsStats: ITrackMonthlyStats[]
 ): IMonthData[] => {
   return tracksLast3MonthsStats.map((monthStats) => {
-    const monthName = monthNames[monthStats.month - 1];
+    const monthName = monthsNames[monthStats.month - 1];
 
     const monthData: IMonthData = {
       month: monthName,
