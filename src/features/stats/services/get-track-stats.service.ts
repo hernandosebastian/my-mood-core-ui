9a2243a3ee753c7237af3540db3f04b5b2a2314e
hernandosebastian <sebastianhernando.dev@gmail.com>
2024-12-19 13:22:33 -0300

    feature: add startDate and updateDate to getTrackStats service with new utils fucntion

--- src/features/stats/services/get-track-stats.service.ts
+++ src/features/stats/services/get-track-stats.service.ts
@@ -2,10 +2,12 @@ import { env } from "@/config/env";
 import { getItem, StorageKeys } from "@/services/local-storage";
 import axios from "axios";
 import { IGetTrackStatsResponse } from "../interfaces";
+import { getStatsStartAndEndDate } from "../utils";
 
 export const getTrackStats = async (): Promise<IGetTrackStatsResponse> => {
+  const { startDate, endDate } = getStatsStartAndEndDate();
   const authToken = getItem(StorageKeys.COGNITO_ACCESS_TOKEN);
-  const apiUrl = `${env.coreApi.baseUrl}/track/stats`;
+  const apiUrl = `${env.coreApi.baseUrl}/track/stats?startDate=${startDate}&endDate=${endDate}`;
 
   const response = await axios.get<IGetTrackStatsResponse>(apiUrl, {
     headers: {
--- src/features/stats/utils/index.ts
+++ src/features/stats/utils/index.ts
@@ -1,1 +1,2 @@
 export { chartConfiguration } from "./chart-configuration.utils";
+export { getStatsStartAndEndDate } from "./get-stats-start-and-end-date.utils";
