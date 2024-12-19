c34608d3d5c7509893cf07225060da2eb809e0d6
hernandosebastian <sebastianhernando.dev@gmail.com>
2024-12-19 13:05:11 -0300

    refactor: update stats page with props and conditional

--- src/features/stats/pages/stats.page.tsx
+++ src/features/stats/pages/stats.page.tsx
@@ -38,21 +38,36 @@ export const StatsPage = (): JSX.Element => {
     return <Loading />;
   }
 
-  return (
+  return trackStats ? (
     <div className="min-h-screen flex flex-col" data-testid="homepage-section">
       <main className="flex-grow flex flex-col items-center justify-center px-4 py-4 bg-gradient-to-b from-background to-secondary/20 gap-12">
         <h1 className="text-5xl font-bold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl text-gray-900 text-center">
           Monthly Mood Distribution
         </h1>
-        <LastThreeMonthsMoodTrackingBar />
+        <LastThreeMonthsMoodTrackingBar
+          lastThreeMonths={trackStats.lastThreeMonths}
+        />
         <h1 className="text-5xl font-bold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl text-gray-900 text-center">
           Mood Tracking Overview
         </h1>
         <div className="flex flex-col sm:flex-row w-full max-w-[1000px] gap-12 justify-center items-center">
-          <TotalMoodTrackingDonut />
-          <CurrentMonthMoodTrackingDonut />
+          <TotalMoodTrackingDonut
+            historyTrackMap={trackStats.historyTrackMap}
+          />
+          <CurrentMonthMoodTrackingDonut
+            currentMonth={trackStats.currentMonth}
+          />
         </div>
       </main>
+    </div>
+  ) : (
+    <div className="min-h-screen flex flex-col" data-testid="homepage-section">
+      <main className="flex-grow flex flex-col items-center justify-center px-4 py-4 bg-gradient-to-b from-background to-secondary/20 gap-12">
+        <h1 className="text-5xl font-bold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl text-gray-900 text-center">
+          Currently there is no data
+        </h1>
+      </main>
+      <button onClick={() => navigate("/track")}>Track Mood</button>
     </div>
   );
 };
