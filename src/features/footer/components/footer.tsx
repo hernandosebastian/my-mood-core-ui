3291f4028167d0ad2c40c03676f4fa21f4b0b220
hernandosebastian <sebastianhernando.dev@gmail.com>
2024-12-28 02:31:19 -0300

    feature: add highlight to texts

--- src/features/footer/components/footer.tsx
+++ src/features/footer/components/footer.tsx
@@ -5,7 +5,9 @@ export function Footer(): JSX.Element {
   return (
     <footer className="py-6 px-4 bg-background-primary border-t border-border-primary">
       <div className="max-w-6xl mx-auto text-center text-sm text-text-primary">
-        <p>© {new Date().getFullYear()} My Mood. All rights reserved.</p>
+        <p className="color-highlight-selection">
+          © {new Date().getFullYear()} My Mood. All rights reserved.
+        </p>
         <div className="mt-2 flex items-center justify-center">
           <a
             href={env.githubProfiles.ownerGithubProfile}
@@ -14,7 +16,9 @@ export function Footer(): JSX.Element {
             className="inline-flex items-center space-x-1 text-text-secondary hover:text-accent-primary fill-text-secondary hover:fill-accent-primary transition-colors gap-1"
           >
             <GithubSVG />
-            <span>Created by {env.githubProfiles.ownerGithubName}</span>
+            <span className="color-highlight-selection">
+              Created by {env.githubProfiles.ownerGithubName}
+            </span>
           </a>
         </div>
       </div>
