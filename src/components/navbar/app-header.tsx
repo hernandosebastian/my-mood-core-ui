4918f225e7c8094379b4efbb1413555c49d1b7f2
hernandosebastian <sebastianhernando.dev@gmail.com>
2024-12-28 02:34:28 -0300

    refactor: delete unnecessary separator

--- src/components/navbar/app-header.tsx
+++ src/components/navbar/app-header.tsx
@@ -1,11 +1,12 @@
-import { Separator } from "@radix-ui/react-separator";
 import { SidebarTrigger } from "../ui/sidebar";
 
 export function AppHeader(): JSX.Element {
   return (
     <header className="sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b bg-background-primary px-4">
-      <SidebarTrigger className="-ml-1" data-testid="toggle-sidebar-trigger" />
-      <Separator orientation="vertical" className="mr-2 h-4" />
+      <SidebarTrigger
+        className="-ml-1 text-text-secondary hover:text-text-primary bg-inherit hover:bg-inherit"
+        data-testid="toggle-sidebar-trigger"
+      />
     </header>
   );
 }
