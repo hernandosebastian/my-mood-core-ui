40d8c21cfa51192be68a5c96048d5ecba96d468c
hernandosebastian <sebastianhernando.dev@gmail.com>
2024-12-28 02:34:58 -0300

    refactor: update border color

--- src/components/navbar/app-header.tsx
+++ src/components/navbar/app-header.tsx
@@ -2,7 +2,7 @@ import { SidebarTrigger } from "../ui/sidebar";
 
 export function AppHeader(): JSX.Element {
   return (
-    <header className="sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b bg-background-primary px-4">
+    <header className="sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b bg-background-primary px-4 border-border-primary">
       <SidebarTrigger
         className="-ml-1 text-text-secondary hover:text-text-primary bg-inherit hover:bg-inherit"
         data-testid="toggle-sidebar-trigger"
