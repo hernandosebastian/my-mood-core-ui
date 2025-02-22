ad1a77b60581b1ed57d4a197387de654bfd450a9
hernandosebastian <sebastianhernando.dev@gmail.com>
2025-02-22 01:14:41 -0300

    refactor: update loading styles

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -4,10 +4,9 @@ export function Loading(): JSX.Element {
   return (
     <div className="flex flex-col items-center justify-center text-center">
       <div className="p-4 rounded-lg flex flex-col gap-6 items-center justify-center">
-        <Loader2 className="h-20 w-20 animate-spin text-gray-600" />
-        <span className="text-gray-800 font-medium">Loading</span>
+        <Loader2 className="h-20 w-20 animate-spin text-text-primary" />
+        <span className="text-text-secondary font-medium">Loading</span>
       </div>
     </div>
   );
 }
-
