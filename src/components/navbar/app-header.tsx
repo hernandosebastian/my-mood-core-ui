320dc26a731dbae25e66d66896cf378761927d72
hernandosebastian <sebastianhernando.dev@gmail.com>
2025-02-22 14:22:07 -0300

    feature: add home icon on header

--- src/components/navbar/app-header.tsx
+++ src/components/navbar/app-header.tsx
@@ -1,12 +1,25 @@
+import { Button } from "../ui/button";
 import { SidebarTrigger } from "../ui/sidebar";
-
+import { HomeIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 export function AppHeader(): JSX.Element {
+  const navigate = useNavigate();
+
   return (
-    <header className="sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b bg-background-primary px-4 border-border-primary">
+    <header className="sticky top-0 flex h-16 shrink-0 items-center gap-6 border-b bg-background-primary px-4 border-border-primary">
       <SidebarTrigger
         className="-ml-1 text-text-secondary hover:text-text-primary transition-colors"
         data-testid="toggle-sidebar-trigger"
       />
+      <Button
+        variant="ghost"
+        size="icon"
+        className="-7 w-7 text-text-secondary hover:text-text-primary transition-colors"
+        onClick={() => navigate("/")}
+      >
+        <HomeIcon />
+        <span className="sr-only">Home</span>
+      </Button>
     </header>
   );
 }
