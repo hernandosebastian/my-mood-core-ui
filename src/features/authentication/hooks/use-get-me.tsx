8215d4bdf51688ef39c38e93c81f87d95d3c44c4
hernandosebastian <sebastianhernando.dev@gmail.com>
2025-02-24 23:15:33 -0300

    chore: improve useGetMe hook to not make infinity requests

--- src/features/authentication/hooks/use-get-me.tsx
+++ src/features/authentication/hooks/use-get-me.tsx
@@ -1,19 +1,33 @@
-import { useQuery, UseQueryResult } from "react-query";
+import { useQuery, UseQueryResult, useQueryClient } from "react-query";
 import { getMe } from "../services";
 import { IGetMeResponse } from "../dto";
-import { getItem, StorageKeys } from "@/services/local-storage";
+import { getItem, removeItem, StorageKeys } from "@/services/local-storage";
 import { mapToUser } from "../mapper";
 import { User } from "../entity";
 import { AxiosError } from "axios";
 
 export const getMeKeys = (): string[] => ["getMe"];
+const MAX_RETRIES = 3;
+const RETRY_DELAY = (attemptIndex: number): number =>
+  Math.min(1000 * 2 ** attemptIndex, 30_000);
 
 export const useGetMe = (): UseQueryResult<{ user: User }, AxiosError> => {
+  const queryClient = useQueryClient();
   const accessToken = getItem(StorageKeys.COGNITO_ACCESS_TOKEN);
+
   return useQuery<IGetMeResponse, AxiosError, { user: User }>({
     queryKey: getMeKeys(),
     queryFn: getMe,
     enabled: !!accessToken,
+    retry: MAX_RETRIES,
+    retryDelay: RETRY_DELAY,
+    onError: (error: AxiosError) => {
+      const isTokenExpired = error.response?.status === 401;
+      if (isTokenExpired) {
+        removeItem(StorageKeys.COGNITO_ACCESS_TOKEN);
+        queryClient.removeQueries(getMeKeys());
+      }
+    },
     select: ({
       id,
       username,
@@ -24,7 +38,7 @@ export const useGetMe = (): UseQueryResult<{ user: User }, AxiosError> => {
       createdAt,
       updatedAt,
       deletedAt,
-    }: IGetMeResponse) => ({
+    }) => ({
       user: mapToUser({
         id,
         username,
