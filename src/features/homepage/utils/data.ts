9d2ed0e9b59770f4635169a89a4480bc9d8a47a5
hernandosebastian <sebastianhernando.dev@gmail.com>
2025-05-24 23:34:58 -0300

    chore: add data about calendar

--- src/features/homepage/utils/data.ts
+++ src/features/homepage/utils/data.ts
@@ -6,13 +6,19 @@ export const whyTrackYourMoodData = [
       "Registrá cómo te sentís con solo unos clics y descubrí cómo cambian tus emociones con el tiempo gracias a estadísticas claras y visuales.",
   },
   {
+    image: "/assets/homepage/emotional-history.png",
+    title: "Desarrollá autoconciencia",
+    description:
+      "Reflexioná día a día sobre tus emociones y empezá a notar patrones, gatillos y momentos clave en tu bienestar emocional.",
+  },
+  {
     image: "/assets/homepage/progress-tracking.png",
     title: "Seguimiento de tu evolución",
     description:
       "Observá tus tendencias emocionales, celebrá avances o detectá momentos donde necesitás prestarte más atención.",
   },
   {
-    image: "/assets/homepage/emotional-history.png",
+    image: "/assets/homepage/calendar-access.png",
     title: "Explorá tu historial emocional",
     description:
       "Accedé fácilmente a los días que registraste desde el calendario principal. Los colores te muestran tu estado de ánimo a lo largo del tiempo de un solo vistazo.",
